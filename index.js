3a7e6ea1a7d0334b0c7f051e2ba31f95be6698a3
shamimspro <shamimspro@gmail.com>
2022-11-18 02:28:19 +0600

    create appointmentOptions public api

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.egsefuu.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {};
+            const result = await appointmentOptionCollection.find(query).toArray();
+            res.send(result);
+        });
+    } catch(error) {
+        console.log(error);
+    }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+    res.send('Server is running fine....');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
